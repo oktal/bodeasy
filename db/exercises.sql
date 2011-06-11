fb2815b87ddc802483db69a1f5de6494205a3a37
Filipe Azevedo <pasnox@gmail.com>
2011-06-11 14:14:58 +0200

    Add test case scripts for exercises/sessions.

--- db/exercises.sql
+++ db/exercises.sql
@@ -1,99 +1,22 @@
-insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('1', '[Grand Droit] - Crunch', '0', 'false', '0', 'But de l''exercice
-Cet exercice de musculation affine et raffermis la taille si vous travaillez avec le poids du corps. Il développe les abdominaux si vous utilisez un lest de plus en plus lourd.
+insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('1', '[Grand Droit] - Crunch', '0', 'false', '0', '');
+insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('2', '[Grand Droit] - Sit-Up', '0', 'false', '0', '');
+insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('3', '[Grand Droit] - Relevés de Jambes', '0', 'false', '0', '');
+insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('4', '[Grand Droit] - Relevé de jambes sur plan incliné', '0', 'false', '0', '');
+insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('5', '[Grand Droit] - Relevé de buste sur banc incliné', '0', 'false', '0', '');
+insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('6', '[Grand Droit] - Relevé de bassin sur plan incliné', '0', 'false', '0', '');
+insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('7', '[Obliques] - Crunch avec Rotation', '0', 'false', '0', '');
+insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('8', '[Obliques] - Relevé de jambes en suspension', '0', 'false', '0', '');
+insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('9', '[Obliques] - Torsion de Bassin couché', '0', 'false', '0', '');
+insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('10', '[Obliques] - Flexions Latérales', '0', 'false', '0', '');
 
-Muscles ciblés
-Il sollicite le grand droit des abdominaux et les obliques.
-Le grand droit ou tablette de chocolat fait partie de la sangle abdominale avec les obliques et le transverse. L’action du grand droit est la flexion du tronc.
+insert into exercise_muscle_group ("id_exercise", "id_muscle_group") values ('1', '1');
+insert into exercise_muscle_group ("id_exercise", "id_muscle_group") values ('2', '1');
+insert into exercise_muscle_group ("id_exercise", "id_muscle_group") values ('3', '1');
+insert into exercise_muscle_group ("id_exercise", "id_muscle_group") values ('4', '1');
+insert into exercise_muscle_group ("id_exercise", "id_muscle_group") values ('5', '1');
+insert into exercise_muscle_group ("id_exercise", "id_muscle_group") values ('6', '1');
+insert into exercise_muscle_group ("id_exercise", "id_muscle_group") values ('7', '1');
+insert into exercise_muscle_group ("id_exercise", "id_muscle_group") values ('8', '1');
+insert into exercise_muscle_group ("id_exercise", "id_muscle_group") values ('9', '1');
+insert into exercise_muscle_group ("id_exercise", "id_muscle_group") values ('10', '1');
 
-Exécution de l’exercice
- Position de départ allongé sur le sol ou sur un banc, les lombaires collés au sol, les jambes peuvent être fléchies sur la poitrine, reposer sur un banc ou former un angle de 90 degrés avec les pieds au sol.
-Enrouler le buste vers l’avant en contractant les abdominaux et en gardant les lombaires collés au sol.
-Le retour à la position de départ doit être réalisé sans à coups en conservant la contraction et la tension dans le muscle. La vitesse d’exécution est lente et constante. On peut placer un lest sur la poitrine, une rondelle de fonte ou un haltère pour ajouter de la difficulté à l’exercice.
-
- Respiration
-Inspiration en début de mouvement quand la cage thoracique est ouverte. Souffler en exécutant le mouvement.
-
-Consignes de sécurité
-Évitez de placer vos mains derrière la tête car souvent on s''aide de la tête pour faire l''exercice quand on bloque, cela peut entraîner des problèmes aux cervicales. Le plus simple est de maintenir les mains sur les tempes ou sur la poitrine.
-Ne pas décoller le bas du dos, l''amplitude très réduite n’enlève pas d’efficacité à l’exercice. 
-Il faut travailler les pieds non bloqués, serrés et les genoux écartés, cela limite l''activité du psoas iliaque et du droit antérieur et évite bien des problèmes de dos.');
-insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('2', '[Grand Droit] - Sit-Up', '0', 'false', '0', 'But de l''exercice
-Cet exercice de musculation raffermis la taille et travaille les abdominaux. Il a la réputation de travailler le bas du ventre la ou le crunch au sol sollicite plutôt le haut des abdominaux.
-
- Muscles ciblés
-Il sollicite le grand droit des abdominaux en statique, le droit antérieur et le psoas iliaque.
-Le grand droit fait partie de la sangle abdominale avec les obliques et le transverse. L’action du grand droit est la flexion du tronc.
-
-Exécution de l’exercice
-Assis au travers du banc ou sur un tapis de sol, mains au bord du banc pour l''équilibre, tendre les jambes puis ramener les genoux vers la poitrine. Garder les abdos sous tension pendant le mouvement.
-
-Respiration
-Inspiration en tendant les jambes.
-
-Consignes de sécurité
-Il faut veiller a ce que les pieds ne touchent jamais le sol. Cet exercice n’a pas grand intérêt car le grand droit est travaillé en statique, c’est surtout le psoas iliaque et le droit antérieur qui sont sollicités. Il vaut mieux faire des crunchs ou des relevés de bassin qui enroulent la colonne vertébrale et sollicitent le grand droit des abdominaux en dynamique.');
-insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('3', '[Grand Droit] - Relevé de Jambes', '0', 'false', '0', 'But de l''exercice
-Cet exercice très similaire à la chaise romaine qu’on trouve en salle de musculation raffermis la taille. Il travaille les abdominaux.
-
-Muscles ciblés
-Il sollicite le grand droit des abdominaux en statique, le droit antérieur et le psoas iliaque.
-Le grand droit fait partie de la sangle abdominale avec les obliques et le transverse. L’action du grand droit est la flexion du tronc.
-
- Exécution de l’exercice
-Pendu à une barre, sur un espalier ou callé sur une chaise romaine, monter les genoux le plus haut possible, contracter fort le grand droit des abdominaux puis redescendre lentement sans cambrer le bas du dos.
-Personnellement, j’aime terminer ce mouvement en enroulant mon bassin et en touchant la barre avec mes pieds. La position finale en boule permet d’étirer tout le haut du dos et sollicite les dorsaux.
-
-Respiration
-Les muscles étant constamment contractés, avoir une respiration aisée est difficile, expirez en contractant fort.
-
-Consignes de sécurité
-Cet exercice similaire à la chaise romaine n’a pas grand intérêt car le grand droit est travaillé en statique, c’est surtout le psoas iliaque et le droit antérieur qui sont travaillés. Il vaut mieux réaliser des crunchs ou des relevés de bassin qui enroulent la colonne vertébrale et sollicitent le grand droit des abdominaux en dynamique.
-Pensez a pratiquer des étirements car le raccourcissement par manque de souplesse du droit antérieur et du psoas iliaque est souvent responsable des douleurs au bas du dos.');
-insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('4', '[Grand Droit] - Relevés de Jambes sur plan incliné', '0', 'false', '0', 'But de l''exercice
-Cet exercice de musculation raffermis la taille. Il travaille les abdominaux.
-
-Muscles ciblés
-Il sollicite le grand droit des abdominaux en statique, le droit antérieur et le psoas iliaque.
-Le grand droit ou "tablette de chocolat" fait partie de la sangle abdominale avec les obliques et le transverse. L’action du grand droit est la flexion du tronc.
-
- Exécution de l’exercice
-Allongé sur un banc incliné, fléchir les jambes légèrement et les ramener vers le tronc en gardant les lombaires collés au banc. L''amplitude de l’exercice est faible, moins de 40°. On ne décolle pas le buste ni le bassin pendant le mouvement. On doit maintenir le buste collé contre le banc a l’aide d’une prise stable comme un espalier ou les montants du banc.
-
-Respiration
-Les muscles étant constamment contractés, avoir une respiration aisée est difficile, expirez en contractant fort quand les jambes sont rabattues.
-
-Consignes de sécurité
-On remarque souvent en salle de musculation que l’exercice est mal exécuté avec une trop grande amplitude et vitesse d’exécution qui fait qu’on décolle les lombaires et qu’on place sa colonne dans une position dangereuse. Suivant l’inclinaison du banc, il faut veiller a ce que les pieds ne touchent jamais le sol. 
-Cet exercice similaire à la "chaise romaine" n’a pas grand intérêt car le grand droit est travaillé en statique, c’est surtout le psoas iliaque et le droit antérieur qui sont sollicités. Il vaut mieux réaliser des crunchs ou des relevés de bassin sur banc incliné qui enroulent la colonne vertébrale et travaillent le grand droit des abdominaux en dynamique.
-Si vous choisissez cet exercice, il faut penser alors a pratiquer des étirements. Le raccourcissement par manque de souplesse du droit antérieur et du psoas iliaque est souvent responsable des douleurs au bas du dos.');
-insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('5', '[Grand Droit] - Relevé de buste sur banc incliné', '0', 'false', '0', 'But de l''exercice
-Cet exercice de musculation travaille les abdominaux. Il raffermis la taille et développe la sangle abdominale si on utilise un lest.
-
-Muscles ciblés
-Il sollicite le grand droit des abdominaux, le droit antérieur, le psoas iliaque et le tenseur du fascia lata. Le grand droit ou tablette de chocolat fait partie de la sangle abdominale avec les obliques et le transverse. L’action du grand droit est la flexion du tronc.
-
- Exécution de l’exercice
-Assis sur le banc avec les pieds bloqués sous les boudins, incliner le tronc de 25° maximum avec le dos légèrement arrondis. Remonter doucement jusqu''à la position de départ. L’amplitude de l’exercice doit être limitée.
-Il est possible pour cet exercice de faire une petite rotation sur le coté en fin de mouvement pour solliciter les obliques.
-
-Respiration
-Inspirez en inclinant le tronc et expirez en fin de mouvement.
-
-Consignes de sécurité
-Cet exercice de musculation, souvent réalisé en bloquant les pieds sous le lit ou un meuble à la maison est aussi pratiqué en salle. Le psoas iliaque et le droit antérieur font le gros du travail, il ne faut donc pas que les pieds soient bloqués pendant le mouvement si vous tenez à votre colonne vertébrale. Si vous choisissez cet exercice, il faut pratiquer des étirements de ces muscles. Le raccourcissement par manque de souplesse du droit antérieur et du psoas iliaque est souvent responsable des douleurs du bas du dos.
-');
-insert into exercise ("id_exercise", "name", "type", "weight", "difficulty", "description") values ('6', '[Grand Droit] - Relevé de bassin sur plan incliné ou au sol', '0', 'false', '0', 'But de l''exercice
-Cet exercice de musculation travaille les abdominaux. Il raffermis la taille mais il est difficile de l’épaissir avec car on ne peux pas rajouter de lest comme avec les crunchs.
-
-Muscles ciblés
-Il sollicite le grand droit des abdominaux.
-Le grand droit, la tablette de chocolat fait partie de la sangle abdominale avec les obliques et le transverse et permet la flexion du tronc. On entend souvent dire que les exercices ou l’on relève le bassin et les jambes en gardant le buste fixe permettent de travailler plus le bas des abdominaux alors que ceux comme les crunchs ou l’on enroule la cage thoracique avec les jambes immobiles travaillent plus le haut des abdominaux.
-
- Exécution de l’exercice
-Deux variantes légèrement différentes sont possibles sur banc incliné ou au sol. Couché sur le banc, jambes fléchies, décoller les fessiers en enroulant le bas du dos et amener les genoux vers la poitrine, revenir en position lentement.
-
-Respiration
-Inspiration en début de mouvement quand la cage thoracique est ouverte.
-
-Consignes de sécurité
-Suivant l’inclinaison du banc, il faut veiller a ce que les pieds ne touchent jamais le sol. Toujours ralentir l’exécution de l’exercice en évitant de cambrer les lombaires quand les jambes redescendent. Pour la version sans banc, mettre un tapis au sol pour éviter de vous faire mal au bas du dos.');
---
+++ db/sessions.sql
@@ -0,0 +1,11 @@
+insert into session ("id_session", "name", "objective") values ('1', 'Lundi', 'Abdo en beton');
+insert into session ("id_session", "name", "objective") values ('2', 'Mardi', 'Abdo oblique en beton');
+
+insert into session_exercise ("id_session_exercise", "id_session", "id_exercise", "series", "repetitions", "rest") values ('1', '1', '1', '4', '10', '60');
+insert into session_exercise ("id_session_exercise", "id_session", "id_exercise", "series", "repetitions", "rest") values ('2', '1', '3', '4', '10', '60');
+insert into session_exercise ("id_session_exercise", "id_session", "id_exercise", "series", "repetitions", "rest") values ('3', '1', '5', '4', '10', '60');
+insert into session_exercise ("id_session_exercise", "id_session", "id_exercise", "series", "repetitions", "rest") values ('4', '1', '6', '4', '10', '60');
+insert into session_exercise ("id_session_exercise", "id_session", "id_exercise", "series", "repetitions", "rest") values ('5', '2', '7', '4', '10', '60');
+insert into session_exercise ("id_session_exercise", "id_session", "id_exercise", "series", "repetitions", "rest") values ('6', '2', '10', '4', '10', '60');
+insert into session_exercise ("id_session_exercise", "id_session", "id_exercise", "series", "repetitions", "rest") values ('7', '2', '9', '4', '10', '60');
+insert into session_exercise ("id_session_exercise", "id_session", "id_exercise", "series", "repetitions", "rest") values ('8', '2', '8', '4', '10', '60');
