a666a75421720cb8f4d25716bcf3841e118bed77
octal <octal@localhost>
2011-06-25 18:17:59 +0200

    added a missing header file

---
+++ src/settings.h
@@ -0,0 +1,24 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <QString>
+
+enum ViewMode { List, Page };
+
+#define SETTING_WINDOW_SAVESTATE "saveWindowState"
+#define SETTING_WINDOW_GEOMETRY "window/geometry"
+#define SETTING_WINDOW_STATE "window/state"
+#define SETTING_START_CHRONO "chronometer/start"
+#define SETTING_RESET_CHRONO "chronometer/reset"
+#define SETTING_STOP_CHRONO "chronometer/stop"
+#define SETTING_AUTO_STARTUP "startup/auto"
+#define SETTING_USER_STARTUP "startup/user"
+#define SETTING_LASTUSER_STARTUP "startup/lastuser"
+#define SETTING_VIEW_MODE "view/mode"
+#define SETTING_VIEW_STYLE "view/style"
+#define SETTING_VIEW_LANGUAGE "view/language"
+#define SETTING_LASTUSERNAME "lastUsername"
+#define SETTING_LASTUSERID "lastUserId"
+
+
+#endif // SETTINGS_H
